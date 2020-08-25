216adeea163bce8129652206db8f9dffd5b388da
Bernard Landon <bernard.landon@gmail.com>
2020-08-25 11:35:44 +0200

    Edit default network interface
    Defaults settings were causing network issues with Vagrant
    when adding static interface.

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -e
 systemctl disable zincati
+nmcli connection delete "Wired Connection"
+nmcli connection add \
+  type ethernet \
+  con-name Eth0 \
+  ifname eth0 \
+  ipv4.method auto \
+  connect.autoconnect yes
+systemctl restart NetworkManager
